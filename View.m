5dfb5989f78a84978213f23d6795b7877808b790
Sunil Gopinath <sxg282@gmail.com>
2012-03-29 02:36:41 -0400

    printing more than hello world

--- View.m
+++ View.m
@@ -16,6 +16,11 @@
     if (self) {
         // Initialization code
         self.backgroundColor = [UIColor yellowColor];
+        //Keep the size of the view the same,
+		//but move the origin to the center of the view.
+		CGFloat w = self.bounds.size.width;
+		CGFloat h = self.bounds.size.height;
+		self.bounds = CGRectMake(-w / 2, -h / 2, w, h);
     }
     return self;
 }
@@ -25,10 +30,18 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    [@"Hello, World!"
-     drawAtPoint: CGPointMake(50, 200)
-     withFont: [UIFont systemFontOfSize: 32]
-     ];
+    // Drawing code
+    UIFont *f = [UIFont systemFontOfSize: 32.0];
+    NSString *s = @"هزا مدهش";
+    UIDevice *device = [UIDevice currentDevice];	//There is only one object of this class.
+    
+	//NSString *string = device.model;		//Is it an iPhone, iPod, or iPad?
+	//NSString *string = device.uniqueIdentifier;	//serial number
+	//NSString *string = device.systemName;		//name of operating system, e.g. "iPhone OS"
+	NSString *string = device.systemVersion;	//version number of operating system, e.g., "4.3"
+    CGSize size = [string sizeWithFont: f];
+    CGPoint point = CGPointMake(-size.width / 2, -size.height / 2);
+    [string drawAtPoint: point withFont: f];
 }
 
 
